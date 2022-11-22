141706990a4756229bd09b6c04c8cae0085ef1ee
Anna Schmidt <marshall1697@gmail.com>
2022-11-22 14:42:16 -0700

    completed authorization/login routes, User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
+    PASSWORD_REQUIREMENTS = /\A
+        (?=.{8,}) #minimum 8 chars long
+        (?=.*\d) #contain at least one number
+        (?=.*[a-z]) #contain at least one lowercase letter
+        (?=.*[A-Z]) #at least one uppercase letter
+        (?=.*[[:^alnum:]]) #at least one symbol
+    /x
+
+    validates :username, :password, :bio, presence: true
+    validates :password, format: { with: PASSWORD_REQUIREMENTS, message: "Must be at least 8 characters, include at least one upper case letter, one lower case letter, one number and one symbol" }
+    validates :username, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :users, only: []
- 
-
+  
+  #unique auth routes
+  post '/signup', to: "users#create"
+  get '/auth', to: "users#show"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 
   #for deployed react-router to work
   get '*path',
