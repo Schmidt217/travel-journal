cee4bb18aea607cd16fbcd41af0365665d5078d7
Anna Schmidt <marshall1697@gmail.com>
2022-11-22 13:22:08 -0700

    React setup complete

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>App Page</h1>
+    </div>
+  );
+}
+export default App;
